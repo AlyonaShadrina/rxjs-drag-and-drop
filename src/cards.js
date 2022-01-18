70c5d56712bc4b33dbe00173530aabb3b4841234
Shadrina Alyona <alyon.shadrina@gmail.com>
2022-01-18 13:24:21 +0200

    add events for visual clone of draggable

--- src/cards.js
+++ src/cards.js
@@ -1,33 +1,55 @@
-import { fromEvent } from 'rxjs';
-import { takeUntil, switchMap, withLatestFrom } from 'rxjs/operators';
+import { fromEvent, merge, from } from 'rxjs';
+import { takeUntil, switchMap, withLatestFrom, bufferCount, map, sequenceEqual, mergeMap, filter } from 'rxjs/operators';
 
 const draggable = document.querySelectorAll('.draggable');
 const droppable = document.querySelectorAll('.droppable');
 
-const mouseDown$ = fromEvent(draggable, 'mousedown');
-const mouseUp$ = fromEvent(document, 'mouseup');
-const mouseMove$ = fromEvent(document, 'mousemove');
-const drop$ = fromEvent(droppable, 'mouseup');
+const mouseDownOnDraggable$ = fromEvent(draggable, 'mousedown');
+const mouseUpOnDocument$ = fromEvent(document, 'mouseup');
+const mouseUpWindow$ = fromEvent(window, 'mouseup');
+const mouseMoveOnDocument$ = fromEvent(document, 'mousemove');
+const mouseUpOnDroppable$ = fromEvent(droppable, 'mouseup');
 
-
-const dragStart$ = mouseDown$;
-const dragMove$ = dragStart$.pipe( // on every mousedown
-  switchMap(() => mouseMove$.pipe( // on every mousemove
-    takeUntil(mouseUp$) // until mouseup
+const dragMove$ = mouseDownOnDraggable$.pipe( // on every mousedown
+  switchMap(() => mouseMoveOnDocument$.pipe( // on every mousemove
+    takeUntil(mouseUpOnDocument$) // until mouseup
   )),
 );
-const dragDrop$ = dragMove$.pipe(
-  switchMap(() => drop$.pipe(
-    takeUntil(mouseUp$) // until mouseup (hoists to document from droppable, so mouseUp triggers after drop)
+const dragDrop$ = dragMove$.pipe( // on every drag move
+  switchMap(() => mouseUpOnDroppable$.pipe( // catch every mouseup on droppable
+    takeUntil(mouseUpOnDocument$) // until mouseup (hoists to document from droppable, so mouseUp triggers after drop)
   )),
 );
+const dragMouseUp$ = dragMove$.pipe( // on every drag move
+  switchMap(() => mouseUpOnDocument$.pipe( // catch every mouseup on document
+    takeUntil(mouseUpWindow$) // until mouseup on window (hoists to window from document)
+  )),
+);
+const dragStart$ = merge(mouseDownOnDraggable$, mouseMoveOnDocument$).pipe( // on every draggable mouse down and document mouse move
+  map(e => e.type), // get event type
+  bufferCount(2, 1), // get two events
+  mergeMap(
+    last2 => from(last2).pipe(sequenceEqual(from(['mousedown','mousemove']))), // check sequence of 'mousedown' + 'mousemove'
+  ),
+  filter(bool => bool), // get only 'mousedown' + 'mousemove' events
+);
 
-dragMove$.subscribe(() => {
+dragStart$.pipe(
+  withLatestFrom(mouseDownOnDraggable$),
+).subscribe(matched => console.log('create clone ', matched));
+dragMove$.pipe(
+  withLatestFrom(mouseDownOnDraggable$),
+).subscribe(([moveEvent, mouseDownEvent]) => {
   window.getSelection().removeAllRanges(); // helps remove bugs when text selected
   // TODO: create visual clone of draggable element 
+  // const clone = mouseDownEvent.target.cloneNode();
+  console.log('update position:', moveEvent);
+});
+dragMouseUp$.subscribe((mouseUpEvent) => {
+  console.log('clear container:', mouseUpEvent);
 });
 dragDrop$.pipe(
-  withLatestFrom(mouseDown$),
+  withLatestFrom(mouseDownOnDraggable$),
 ).subscribe(([dropToElement, draggedElement]) => {
   dropToElement.target.closest('.droppable').appendChild(draggedElement.target)
 });
